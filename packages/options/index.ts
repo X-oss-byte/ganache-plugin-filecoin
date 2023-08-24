7d9851d475c38e30a950d766dcd1bc122f8ca0b8
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-08-24 18:50:23 -0400

    chore: move to separate repo
    Co-authored-by: Mike Seese <seesemichaelj@gmail.com>

---
+++ packages/options/index.ts
@@ -0,0 +1,14 @@
+/*!
+ * @ganache/filecoin-options
+ *
+ * @author Tim Coulter
+ * @license MIT
+ */
+
+export {
+  FilecoinInternalOptions,
+  FilecoinOptionsConfig,
+  FilecoinProviderOptions,
+  FilecoinDefaults,
+  FilecoinLegacyProviderOptions
+} from "./src";
